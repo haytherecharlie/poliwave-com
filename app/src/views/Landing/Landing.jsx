dafecfd0b65786a147abc0f24bdb9112c45e3211
charliehay <charlie.hay@telus.com>
2019-10-22 18:18:25 -0400

    feat(poly-4): url preview

--- app/src/views/Landing/Landing.jsx
+++ app/src/views/Landing/Landing.jsx
@@ -6,12 +6,12 @@ import * as S from './styles/landing.styled'
 const Landing = () => (
   <S.Wrapper>
     <Helmet>
-      <title>{`Poliwave | Politics For a New Generation`}</title>
-      <link rel="canonical" href={`https://poliwave.com`} />
-      <meta property="og:title" content={`Poliwave | Politics For a New Generation`} />
-      {/* <meta property="og:image" content={file.url} /> */}
-      <meta property="og:description" content={`Poliwave brings to market a refreshed portfolio of political articles and thought pieces helping you to understand the modern political environment.`} />
-      <meta property="og:url" content={`https://poliwave.com`} />
+      <title>Poliwave | Politics For a New Generation</title>
+      <link rel="canonical" href="https://poliwave.com" />
+      <meta property="og:title" content="Poliwave | Politics For a New Generation" />
+      <meta property="og:image" content="https://github.com/haytherecharlie/poliwave.com/blob/master/app/src/assets/images/poliwave.jpg" />
+      <meta property="og:description" content="Poliwave brings to market a refreshed portfolio of political articles and thought pieces helping you to understand the modern political environment." />
+      <meta property="og:url" content="https://poliwave.com" />
       <meta name="twitter:card" content="summary_large_image" />
     </Helmet>
     <EntriesList />
