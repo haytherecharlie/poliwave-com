517317b0afd4ea32d1b6a8aa78d172bdd87c7f6b
charliehay <charlie.hay@telus.com>
2019-10-22 18:16:17 -0400

    feat(poly-3): helmet

--- app/src/views/Entry/Entry.jsx
+++ app/src/views/Entry/Entry.jsx
@@ -2,26 +2,33 @@ import React, { useEffect } from 'react'
 import { useSelector } from 'react-redux'
 import { withRouter } from 'react-router-dom'
 import { format } from 'date-fns'
+import { Helmet } from 'react-helmet'
 import ReactMarkdown from 'react-markdown'
 import * as S from './styles/entry.styled'
 
-const Entry = ({ match: { params }, history }) => {
-  useEffect(() => {
-    window.scrollTo(0, 0)
-  }, [])
-
+const Entry = ({ match: { params }, location: {pathname}, history }) => {
+  useEffect(() => { window.scrollTo(0, 0) }, [])
   const { entries } = useSelector(s => ({ entries: s.entries }))
-
   const renderEntry = () => {
     const entry = entries.content.items.find(item => item.fields.slug === params.entry)
 
     if (entry) {
-      const { title, author, banner, body } = entry.fields
+      const { title, author, cta, banner, body } = entry.fields
       const { file, description } = banner.fields
       const { createdAt, updatedAt } = entry.sys
       const date = createdAt >= updatedAt ? createdAt : updatedAt
+      console.log(cta)
       return (
         <S.Card>
+          <Helmet>
+            <title>{`Poliwave | ${title}`}</title>
+            <link rel="canonical" href={`https://poliwave.com${pathname}`} />
+            <meta property="og:title" content={title} />
+            <meta property="og:image" content={file.url} />
+            <meta property="og:description" content={cta} />
+            <meta property="og:url" content={`https://poliwave.com${pathname}`} />
+            <meta name="twitter:card" content="summary_large_image" />
+          </Helmet>
           <S.Title>{title}</S.Title>
           <S.Meta>
             {author} | {format(date, 'MMMM Do YYYY')}
--- app/src/views/Landing/Landing.jsx
+++ app/src/views/Landing/Landing.jsx
@@ -1,9 +1,19 @@
 import React from 'react'
+import { Helmet } from 'react-helmet'
 import EntriesList from 'components/EntriesList/EntriesList'
 import * as S from './styles/landing.styled'
 
 const Landing = () => (
   <S.Wrapper>
+    <Helmet>
+      <title>{`Poliwave | Politics For a New Generation`}</title>
+      <link rel="canonical" href={`https://poliwave.com`} />
+      <meta property="og:title" content={`Poliwave | Politics For a New Generation`} />
+      {/* <meta property="og:image" content={file.url} /> */}
+      <meta property="og:description" content={`Poliwave brings to market a refreshed portfolio of political articles and thought pieces helping you to understand the modern political environment.`} />
+      <meta property="og:url" content={`https://poliwave.com`} />
+      <meta name="twitter:card" content="summary_large_image" />
+    </Helmet>
     <EntriesList />
   </S.Wrapper>
 )
